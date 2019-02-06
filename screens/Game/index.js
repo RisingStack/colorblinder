5cbcc88388a49717effdca0180280cc38e1da2bc
Daniel <dandesz198@gmail.com>
2019-02-06 16:20:52 +0100

    Added bottom bar, pause/continue and lose/replay

--- screens/Game/index.js
+++ screens/Game/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Dimensions, TouchableOpacity } from "react-native";
+import { View, Dimensions, Text, Image, TouchableOpacity } from "react-native";
 import { Header } from "../../components";
 import { generateRGB, mutateRGB } from "../../utilities";
 import styles from "./styles";
@@ -7,15 +7,22 @@ import styles from "./styles";
 export default class Home extends Component {
   state = {
     points: 0,
-    timeLeft: 0,
+    timeLeft: 5,
     rgb: generateRGB(),
-    size: 2
+    size: 2,
+    gameState: "INGAME" // three possible states: 'INGAME', 'PAUSED' and 'LOST'
   };
 
   componentWillMount() {
     this.generateNewRound();
     this.interval = setInterval(() => {
-      this.setState({ timeLeft: this.state.timeLeft - 1 });
+      if (this.state.gameState === "INGAME") {
+        if (this.state.timeLeft === 0) {
+          this.setState({ gameState: "LOST" });
+        } else {
+          this.setState({ timeLeft: this.state.timeLeft - 1 });
+        }
+      }
     }, 1000);
   }
 
@@ -41,19 +48,48 @@ export default class Home extends Component {
 
   onTilePress = (rowIndex, columnIndex) => {
     const { diffTileIndex, points, timeLeft } = this.state;
-    if(rowIndex == diffTileIndex[0] && columnIndex == diffTileIndex[1]) {
+    if (rowIndex == diffTileIndex[0] && columnIndex == diffTileIndex[1]) {
       // good tile
-      this.setState({ points: points + 1, timeLeft: timeLeft + 3 });
+      this.setState({ points: points + 1, timeLeft: timeLeft + 3 });
       this.generateNewRound();
     } else {
       // wrong tile
       this.setState({ timeLeft: timeLeft - 1 });
     }
-  }
+  };
+
+  onBottomBarPress = () => {
+    switch (this.state.gameState) {
+      case "INGAME": {
+        this.setState({ gameState: "PAUSED" });
+        break;
+      }
+      case "PAUSED": {
+        this.setState({ gameState: "INGAME" });
+        break;
+      }
+      case "LOST": {
+        this.setState({
+          gameState: "INGAME",
+          points: 0,
+          timeLeft: 15,
+          size: 2
+        });
+        break;
+      }
+    }
+  };
 
   render() {
-    const { rgb, size, diffTileIndex, diffTileColor } = this.state;
+    const { rgb, size, diffTileIndex, diffTileColor, gameState } = this.state;
     const { height } = Dimensions.get("window");
+    const bottomIcon =
+      gameState === "INGAME"
+        ? require("../../assets/icons/pause.png")
+        : gameState === "PAUSED"
+        ? require("../../assets/icons/play.png")
+        : require("../../assets/icons/replay.png");
+
     return (
       <View style={styles.container}>
         <Header />
@@ -64,31 +100,82 @@ export default class Home extends Component {
             flexDirection: "row"
           }}
         >
-          {Array(size)
-            .fill()
-            .map((val, columnIndex) => (
-              <View
-                style={{ flex: 1, flexDirection: "column" }}
-                key={columnIndex}
-              >
-                {Array(size)
-                  .fill()
-                  .map((val, rowIndex) => (
-                    <TouchableOpacity
-                      key={`${rowIndex}.${columnIndex}`}
-                      style={{
-                        flex: 1,
-                        backgroundColor:
-                        rowIndex == diffTileIndex[0] && columnIndex == diffTileIndex[1]
-                            ? diffTileColor
-                            : `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`,
-                        margin: 2
-                      }}
-                      onPress={() => this.onTilePress(rowIndex, columnIndex)}
-                    />
-                  ))}
-              </View>
-            ))}
+          {gameState === "INGAME" ? (
+            Array(size)
+              .fill()
+              .map((val, columnIndex) => (
+                <View
+                  style={{ flex: 1, flexDirection: "column" }}
+                  key={columnIndex}
+                >
+                  {Array(size)
+                    .fill()
+                    .map((val, rowIndex) => (
+                      <TouchableOpacity
+                        key={`${rowIndex}.${columnIndex}`}
+                        style={{
+                          flex: 1,
+                          backgroundColor:
+                            rowIndex == diffTileIndex[0] &&
+                            columnIndex == diffTileIndex[1]
+                              ? diffTileColor
+                              : `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`,
+                          margin: 2
+                        }}
+                        onPress={() => this.onTilePress(rowIndex, columnIndex)}
+                      />
+                    ))}
+                </View>
+              ))
+          ) : gameState === "PAUSED" ? (
+            <View style={styles.pausedContainer}>
+              <Image
+                source={require("../../assets/icons/mug.png")}
+                style={styles.pausedIcon}
+              />
+              <Text style={styles.pausedText}>COVFEFE BREAK</Text>
+            </View>
+          ) : (
+            <View style={styles.pausedContainer}>
+              <Image
+                source={require("../../assets/icons/dead.png")}
+                style={styles.pausedIcon}
+              />
+              <Text style={styles.pausedText}>U DED</Text>
+            </View>
+          )}
+        </View>
+        <View style={styles.bottomContainer}>
+          <View style={{ flex: 1 }}>
+            <Text style={styles.counterCount}>{this.state.points}</Text>
+            <Text style={styles.counterLabel}>points</Text>
+            <View style={styles.bestContainer}>
+              <Image
+                source={require("../../assets/icons/trophy.png")}
+                style={styles.bestIcon}
+              />
+              <Text style={styles.bestLabel}>0</Text>
+            </View>
+          </View>
+          <View style={{ flex: 1 }}>
+            <TouchableOpacity
+              style={{ alignItems: "center" }}
+              onPress={this.onBottomBarPress}
+            >
+              <Image source={bottomIcon} style={styles.bottomIcon} />
+            </TouchableOpacity>
+          </View>
+          <View style={{ flex: 1 }}>
+            <Text style={styles.counterCount}>{this.state.timeLeft}</Text>
+            <Text style={styles.counterLabel}>seconds left</Text>
+            <View style={styles.bestContainer}>
+              <Image
+                source={require("../../assets/icons/clock.png")}
+                style={styles.bestIcon}
+              />
+              <Text style={styles.bestLabel}>0</Text>
+            </View>
+          </View>
         </View>
       </View>
     );
--- screens/Game/styles.js
+++ screens/Game/styles.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
 
 export default StyleSheet.create({
   container: {
@@ -6,5 +6,59 @@ export default StyleSheet.create({
     backgroundColor: "#0a0a0a",
     justifyContent: "center",
     alignItems: "center",
+  },
+  bottomContainer: {
+    width: Dimensions.get("window").height / 2.5,
+    position: 'absolute',
+    bottom: 40,
+    flexDirection: 'row'
+  },
+  bottomIcon: {
+    width: 50,
+    height: 50,
+  },
+  counterCount: {
+    fontFamily: 'dogbyte',
+    textAlign: 'center',
+    color: '#eee',
+    fontSize: 50
+  },
+  counterLabel: {
+    fontFamily: 'dogbyte',
+    textAlign: 'center',
+    color: '#bbb',
+    fontSize: 20
+  },
+  bestContainer: {
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  bestIcon: {
+    width: 25,
+    height: 25,
+    marginRight: 5
+  },
+  bestLabel: {
+    fontFamily: 'dogbyte',
+    color: '#bbb',
+    fontSize: 25,
+    marginTop: 2.5,
+  },
+  pausedContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  pausedText: {
+    fontFamily: 'dogbyte',
+    textAlign: 'center',
+    color: '#eee',
+    marginTop: 20,
+    fontSize: 60,
+  },
+  pausedIcon: {
+    width: 80,
+    height: 80
   }
 });
