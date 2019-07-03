69941964add24ad98fcb7620e96506e80508b691
Daniel Gergely <dandesz198@gmail.com>
2019-07-03 13:21:07 +0200

    🚘 Native animation drivers

--- utilities/shakeAnimation.js
+++ utilities/shakeAnimation.js
@@ -4,21 +4,26 @@ export const shakeAnimation = value => Animated.sequence([
   Animated.timing(value, {
     toValue: 50,
     duration: 100,
+    useNativeDrivers: true,
   }),
   Animated.timing(value, {
     toValue: -50,
     duration: 100,
+    useNativeDrivers: true,
   }),
   Animated.timing(value, {
     toValue: 50,
     duration: 100,
+    useNativeDrivers: true,
   }),
   Animated.timing(value, {
     toValue: -50,
     duration: 100,
+    useNativeDrivers: true,
   }),
   Animated.timing(value, {
     toValue: 0,
     duration: 100,
+    useNativeDrivers: true,
   }),
 ]).start();
